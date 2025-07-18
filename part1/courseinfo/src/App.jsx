1c27234b7b8a63e45a4b033b73867c5bfc078ae9
adfgl <pavel.zhukov.engineer@gmail.com>
2025-07-18 11:53:03 +0300

    1.2 course info

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,28 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+const Header = ({course}) => <h1>{course}</h1>
+const Part = ({part, exercise}) => <p>{part} {exercise}</p>
+const Total = ({parts}) => {
+  const total =  parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      {parts.map((item, index) => (
+        <Part key={index} part={item.part} exercise={item.exercises}/>
+      ))}
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+  const parts = [
+    { part: 'Fundamentals of React', exercises: 10 },
+    { part: 'Using props to pass data', exercises: 7 },
+    { part: 'State of a component', exercises: 14 },
+  ]
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
