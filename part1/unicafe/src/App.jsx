7c511355b4dcdb24956c627692bff449ad9987ee
adfgl <pavel.zhukov.engineer@gmail.com>
2025-07-18 09:55:30 +0300

    add average

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -10,9 +10,9 @@ const Button = ({name, onClick}) => {
   )
 }
 
-const Display = ({name, count}) => {
+const Display = ({name, count, units}) => {
   return (
-    <p>{name} {count}</p>
+    <p>{name} {count}{units === undefined ? "" : units}</p>
   )
 } 
 
@@ -23,6 +23,10 @@ const Header = ({content}) => {
 }
 
 function App() {
+  const scoreGood = 1;
+  const scoreNeutral = 0;
+  const scoreBad = -1;
+
   const nameGood = "good"
   const nameNeutral = "neutral"
   const nameBad = "bad"
@@ -30,18 +34,48 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [average, setAverage] = useState(0.0)
+
+  const calcAverage = (good, neutral, bad) => {
+    const count = good + neutral + bad;
+    if (count === 0) return 0;
+    return (
+      good * scoreGood + 
+      neutral * scoreNeutral +
+      bad * scoreBad
+    ) / count;
+  }
+
+  const handleGood = () => {
+    const updated = good + 1;
+    setGood(updated)
+    setAverage(calcAverage(updated, neutral, bad))
+  }
+
+  const handleNeutral = () => {
+    const updated = neutral + 1;
+    setNeutral(updated);
+    setAverage(calcAverage(good, updated, bad));
+  };
+
+  const handleBad = () => {
+    const updated = bad + 1;
+    setBad(updated);
+    setAverage(calcAverage(good, neutral, updated));
+  };
 
   return (
     <div>
       <Header content={"Give feedback"}/>
-      <Button name={nameGood} onClick={() => setGood(good + 1)}/>
-      <Button name={nameNeutral} onClick={() => setNeutral(neutral + 1)}/>
-      <Button name={nameBad} onClick={() => setBad(bad + 1)}/>
+      <Button name={nameGood} onClick={handleGood}/>
+      <Button name={nameNeutral} onClick={handleNeutral}/>
+      <Button name={nameBad} onClick={handleBad}/>
       
       <Header content={"Statistics"}/>
       <Display name={nameGood} count={good}/>
       <Display name={nameNeutral} count={neutral}/>
       <Display name={nameBad} count={bad}/>
+      <Display name={"average"} count={average}/>
     </div>
   )
 }
